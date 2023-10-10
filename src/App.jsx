e3157a9a4a59cb5e0e018524fa1e054ab3b88565
Claudiu Sonica <csonica95@gmail.com>
2023-10-10 23:09:56 +0300

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Const from "./components/molecules/const/Const";
 import Header from "./components/molecules/header/Header";
+import Footer from "./components/molecules/footer/Footer";
 
 const App = () => {
   return (
     <>
       <Header />
       <Const />
+      <Footer />
     </>
   )
 };
--- src/components/molecules/footer/Footer.jsx
+++ src/components/molecules/footer/Footer.jsx
@@ -1,2 +1,18 @@
 import "./Footer.scss";
 
+import cookies from "/images/cookies.svg";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        Come to the dark side... we have{" "}
+        <span>
+          <img src={cookies} alt="cookies" />
+        </span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;

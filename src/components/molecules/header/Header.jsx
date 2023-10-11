703edd27b7138093843233c78e50f4f744ee0bf4
Claudiu Sonica <csonica95@gmail.com>
2023-10-11 20:50:15 +0300

    fixed button apperence according to pathname

--- src/components/molecules/header/Header.jsx
+++ src/components/molecules/header/Header.jsx
@@ -1,18 +1,23 @@
 import "./Header.scss";
 
+import { Link, useLocation } from "react-router-dom";
+
 import Button from "../../atoms/button/Button";
 import Logo from "../../atoms/logo/Logo";
 import Title from "../../atoms/title/Title";
-import { Link } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <Logo />
       <Title />
-      <Link to="/form">
-        <Button>Join Us</Button>
-      </Link>
+      {location.pathname === "/" && (
+        <Link to="/form">
+          <Button>Join Us</Button>
+        </Link>
+      )}
     </header>
   );
 };
